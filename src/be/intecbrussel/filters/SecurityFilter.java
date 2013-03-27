e6b0ccaf249101703e375187cd8fda22b6ad2519
Kevin Vanbockryck <maintainer@vanbockryck.net>
2013-03-27 15:59:00 +0100

    Started commenting Security

--- src/be/intecbrussel/filters/SecurityFilter.java
+++ src/be/intecbrussel/filters/SecurityFilter.java
@@ -21,9 +21,14 @@ import be.intecbrussel.util.Security;
 
 @WebFilter("/SecretPart")
 public class SecurityFilter implements Filter {
-
+	
+	// Om een gebruiker in te loggen gaan we moeten controleren
+	// of de gebruiker bestaat in de gebruikers DB. Hier zorgen 
+	// we ervoor dat we over een UserDAO beschikken.
 	private final UserDAO userDAO = new UserDAO();
 
+	// Met deze utility Class kunnen we een veilige hash genereren
+	// voor het paswoord van een gebruiker.
 	private final Security security = new Security();
 	
 	public SecurityFilter() {
@@ -37,21 +42,38 @@ public class SecurityFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 		
+		// Als de security check faalt gaan we redirecten naar een login
+		// venster met de volgende URL.
 		final String REDIRECT_URL = request.getServletContext()
 				.getContextPath() + "/UserServlet";
 
+		// Met deze boolean geven we aan of er al dan niet een cookie 
+		// aanwezig is die de gebruiker toegang geeft tot het beveiligd
+		// onderdeel.
 		boolean securityCookieOK = false;
 		
+		// Met deze boolean geven we aan of een gebruikersnaam gevonden is
+		// in de gebruikers DB
 		boolean found = false;
 
+		// Met deze boolean geven we aan of die gebruiker ook toegang krijgt
+		// tot het beveiligd deel (dat het meegegeven paswoord ook correct is)
 		boolean acces = false;
 		
+		// In de filter krijg je een request object mee van het type ServletRequest.
+		// Omdat we hier zeker weten dat we te maken hebben met een HttpServletRequest
+		// casten wij de request naar een HttpServletRequest zodat we toegang hebben
+		// tot al de methodes van HttpServletRequest.
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 
+		// Analoog met request
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
 		
+		// Een van de methodes die we nu kunnen gebruiken van HttpServletRequest is
+		// 'getSession()'. Hiermee vragen we nu de session op van de gebruiker.
 		HttpSession session = httpRequest.getSession();
 		
+		// Initieel geven we 
 		session.setAttribute("userNotFound", found);
 		
 		session.setAttribute("WrongPassword", acces);
